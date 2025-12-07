28edc2040210fb17cf5db4f2debb6af8b75262de
Ilyavosky <ilyacortesruiz@gmail.com>
2025-12-06 22:49:17 -0600

    Implementar la base local

--- app/src/main/java/com/ilya/examenpractico4aunidad/database/AppDatabase.kt
+++ app/src/main/java/com/ilya/examenpractico4aunidad/database/AppDatabase.kt
@@ -2,13 +2,16 @@ package com.ilya.examenpractico4aunidad.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.ilya.examenpractico4aunidad.models.Character
+import androidx.room.TypeConverters
+import com.ilya.examenpractico4aunidad.models.Pokemon
+import com.ilya.examenpractico4aunidad.models.PokemonConverters
 
 @Database(
-    entities = [Character::class],
+    entities = [Pokemon::class],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(PokemonConverters::class)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun characterDao(): CharacterDao
+    abstract fun pokemonDao(): PokemonDao
 }
