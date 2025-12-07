5538f644d082061776d3b98c0aa58feb6dde69a9
Ilyavosky <ilyacortesruiz@gmail.com>
2025-12-07 09:13:02 -0600

    Actualizar utils

--- app/src/main/java/com/ilya/examenpractico4aunidad/utils/Constants.kt
+++ app/src/main/java/com/ilya/examenpractico4aunidad/utils/Constants.kt
@@ -2,10 +2,11 @@ package com.ilya.examenpractico4aunidad.utils
 
 class Constants {
     companion object {
-        const val BASE_URL: String = "https://stand-by-me.herokuapp.com/"
+        const val BASE_URL: String = "https://pokeapi.co/api/v2/"
         const val HOME_ROUTE = "HomeView"
         const val DETAILS_VIEW = "DetailsView"
         const val SEARCH_VIEW = "SearchView"
         const val FAVORITES_VIEW = "FavoritesView"
+        const val TEAM_BUILDER_VIEW = "TeamBuilderView"
     }
 }
